c2940ba479f993fe4015a3092523d291689d8213
Maximilian Fischer <mafis90@gmail.com>
2013-10-24 12:10:34 +0200

    More Bootstrap Love for Buttons & NavBar

--- samples/SingleTenantWebApp/App_Start/BundleConfig.cs
+++ samples/SingleTenantWebApp/App_Start/BundleConfig.cs
@@ -26,7 +26,6 @@ namespace BrockAllen.MembershipReboot.Mvc.App_Start
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
                       "~/Content/bootstrap-theme.css"));
         }
     }
